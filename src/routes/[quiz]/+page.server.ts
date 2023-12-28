214553065bb70163da2f7cf7fce66cc0bb5a6b76
Raymond Ottun <sayjava@gmail.com>
2023-12-28 16:13:30 +0000

    chore: pre-render and publish

--- src/routes/[quiz]/+page.server.ts
+++ src/routes/[quiz]/+page.server.ts
@@ -1,8 +1,16 @@
-import { loadQuizModule } from "$lib/quiz"
+import { loadQuizModule, loadQuizModulesMetadata } from "$lib/quiz"
 
 export const load = async ({ params }) => {
     const module = await loadQuizModule(`quizzes/${params.quiz}`)
     return {
         module,
     }
+}
+
+export const entries = async () => {
+    const modules = await loadQuizModulesMetadata('./quizzes')
+    console.log(modules.map(module => module.path))
+    return modules.map(module => ({
+        quiz: module.path ?? '',
+    }))
 }
