a81061858cbc7352a12aa154d9c37a75a9b672e4
Raymond Ottun <sayjava@gmail.com>
2023-12-28 16:45:01 +0000

    chore: fix deploy

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,8 @@ const config = {
 			outputDir: 'build'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : '/revis3'
 		}
 	}
 };
